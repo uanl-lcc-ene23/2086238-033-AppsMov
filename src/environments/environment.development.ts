ab711049bb07fd5d6e13759983b94b81cad65807
Razielib07 <103334290+Razielib07@users.noreply.github.com>
2023-04-26 00:29:40 -0600

    miapp_Sfotipy

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  firebase: {
+    projectId: 'miapp-2086238',
+    appId: '1:873400479712:web:3d7ac2c7a248efa8f40585',
+    storageBucket: 'miapp-2086238.appspot.com',
+    apiKey: 'AIzaSyCsykjLC_bgoHD0q7nSc6tb6FzV8jRBbUs',
+    authDomain: 'miapp-2086238.firebaseapp.com',
+    messagingSenderId: '873400479712',
+  },};
